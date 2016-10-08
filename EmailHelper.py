409df714207208404dc15ef5eda269854483f786
David Swigart <dswigart@gmail.com>
2016-10-08 13:25:55 -0700

    email helper refine 2.0

--- EmailHelper.py
+++ EmailHelper.py
@@ -31,7 +31,7 @@ class EmailHelper:
         for obj in applicant_queryset:
             message = ''
             message += 'Dear %s,\n\n' % obj.first_name
-            message += '%s\n' % fields.content_body
+            message += '%s\n\n' % fields.content_body
             message += self._process_confirmation_link(obj.code)
             message += '\n\nCheers,\n\nBrian McWhorter, music director\nSarah Viens, trumpet/administration\nwww.orchestranext.com'
             email = EmailMessage(fields.subject_line, message, 'orchestranext@gmail.com',[obj.email_address])
